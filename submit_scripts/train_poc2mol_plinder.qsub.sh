582b9152e02d03c34f23e69b2fbfdf19219da342
judewells <judewells@hotmail.com>
2025-03-23 19:47:13 +0000

    qsub scripts

---
+++ submit_scripts/train_poc2mol_plinder.qsub.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Train Poc2Mol on plinder
+
+#$ -l tmem=127G
+# -l h_vmem=64G
+#$ -l gpu=true
+#$ -l gpu_type=(a40|a100|a100_80)
+#$ -l h_rt=71:55:30
+#$ -S /bin/bash
+#$ -N poc2mol
+#$ -t 1
+#$ -o /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/qsub_logs/
+#$ -wd /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/
+#$ -j y
+date
+hostname
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "####################  QSUB SCRIPT END  ####################"
+conda activate vox
+which python
+ROOT_DIR='/SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/'
+export HYDRA_FULL_ERROR=1
+export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
+cd $ROOT_DIR
+python ${ROOT_DIR}/src/train.py \
++experiment=train_poc2mol_plinder \
+ckpt_path=null \
+data.config.batch_size=12 \
+
+date
---
+++ submit_scripts/train_vox2smiles_combined.qsub.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Train ProFam
+
+#$ -l tmem=127G
+# -l h_vmem=64G
+#$ -l gpu=true
+#$ -l gpu_type=(a40|a100|a100_80)
+#$ -l h_rt=71:55:30
+#$ -S /bin/bash
+#$ -N vox2smiCkpt
+#$ -t 1
+#$ -o /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/qsub_logs/
+#$ -wd /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/
+#$ -j y
+date
+hostname
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "####################  QSUB SCRIPT END  ####################"
+conda activate vox
+which python
+ROOT_DIR='/SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/'
+export HYDRA_FULL_ERROR=1
+export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
+cd $ROOT_DIR
+python src/train.py \
++experiment=train_vox2smiles_combined \
+data.num_workers=0 \
+data.config.batch_size=2 \
+trainer.val_check_interval=5000 \
+ckpt_path = /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/logs/vox2smilesZincAndPoc2MolOutputs/runs/2025-03-22_21-18-58/checkpoints/last.ckpt
+date
