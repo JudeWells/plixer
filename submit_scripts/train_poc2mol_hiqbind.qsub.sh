b7d0e9e82039c6810e1545d0f01b6ede05ee299f
judewells <judewells@hotmail.com>
2025-04-21 21:14:54 +0100

    submit script

--- submit_scripts/train_poc2mol_hiqbind.qsub.sh
+++ submit_scripts/train_poc2mol_hiqbind.qsub.sh
@@ -5,6 +5,7 @@
 #$ -l tmem=127G
 # -l h_vmem=64G
 #$ -l gpu=true
+#$ -R y
 #$ -l gpu_type=(a40|a100|a100_80)
 #$ -l h_rt=91:55:30
 #$ -S /bin/bash
@@ -27,6 +28,6 @@ cd $ROOT_DIR
 python ${ROOT_DIR}/src/train.py \
 experiment=train_poc2mol_hiqbind \
 ckpt_path=null \
-data.num_workers=5 \
-data.config.batch_size=5
+data.num_workers=0 \
+data.config.batch_size=10
 date
