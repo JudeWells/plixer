3522335e7deddbf8790502a4ea4841b4e2b46535
judewells <judewells@hotmail.com>
2025-04-23 15:50:58 +0100

    checkpoint config

--- submit_scripts/train_vox2smiles_combined_hiqbind_CKPT.qsub.sh
+++ submit_scripts/train_vox2smiles_combined_hiqbind_CKPT.qsub.sh
@@ -9,7 +9,7 @@
 #$ -R y
 #$ -l h_rt=91:55:30
 #$ -S /bin/bash
-#$ -N CombiNoCkpt
+#$ -N CombiCkpt1
 #$ -t 1
 #$ -o /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/qsub_logs/
 #$ -wd /SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/
@@ -28,7 +28,7 @@ cd $ROOT_DIR
 python src/train.py \
 experiment=train_vox2smiles_combined_hiqbind \
 data.num_workers=0 \
-data.config.batch_size=4 \
+data.config.batch_size=8 \
 task_name="CombinedHiQBindCkptFrmPrevCombined" \
 trainer.accumulate_grad_batches=4 \
 ckpt_path="logs/vox2smilesZincAndPoc2MolOutputs/runs/2025-03-22_21-18-58/checkpoints/last.ckpt"
