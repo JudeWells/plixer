1bbfba23432f75ad556ed605b91b15441d109741
judewells <judewells@hotmail.com>
2025-05-07 18:48:37 +0100

    vis test

--- submit_scripts/visualise_voxels_in_test_set.qsub.sh
+++ submit_scripts/visualise_voxels_in_test_set.qsub.sh
@@ -22,5 +22,5 @@ ROOT_DIR='/SAN/orengolab/nsp13/VoxelDiffOuter/VoxelDiff2/'
 export HYDRA_FULL_ERROR=1
 export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
 cd $ROOT_DIR
-python ${ROOT_DIR}/scripts/adhoc_analysis/visualise_voxels_in_test_set.py --task_idx $SGE_TASK_ID --num_tasks $SGE_TASK_LAST
+python ${ROOT_DIR}/scripts/adhoc_analysis/visualise_voxels_in_test_set.py --task_idx $(($SGE_TASK_ID - 1)) --num_tasks $SGE_TASK_LAST
 date
